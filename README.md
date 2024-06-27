a3044c9fa25c92473c8d13a37034dad97b58cca7
chrishart0 <christianphart0@gmail.com>
2024-06-27 15:13:06 -0400

    Co-authored-by: smnzrn <smnzrn@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,63 @@
+Relevant learning materials
+* Intro to building with GenAI using LangChain and Open AI <https://learn.deeplearning.ai/langchain/lesson/1/introduction>
+* https://learn.deeplearning.ai/langchain-chat-with-your-data/lesson/1/introduction
+
+
+![Basic LangChain Agent](images/basic-agent-use.gif)
+
+## Setup
+### 1) Prepare the ollama
+* Follow the official docs to get setup: <https://github.com/ollama/ollama>
+* Ensure you have the needed model pulled down
+
+You can check the .env file to see what model is specified, you will need to ensure that model is pulled down. 
+```
+ollama pull llama3:8b-text-fp16
+# ollama pull llama3
+```
+
+#### Configure your .env file as needed
+* Ensure the `MODEL` defined is one you have downloaded with ollama
+    * NOTE: The `mistral` model provided by ollama is really `7b-instruct-v0.2-q4_0`. This model is blazing fast, but isn't that smart and will fail on anything but simple questions.
+        * This may change in the future as ollama updates their defaults
+    * For higher quality outputs, try updating the model to `mixtral`, note this will be slower
+* Make sure the URL specified is correct for your setup of Ollama
+
+### 2) Install prereqs
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt --upgrade
+```
+
+## Spin up a qdrant vector db 
+This is needed for some of the more advanced examples, such as example 3. Example 2 uses qdrant in memory. 
+
+docker run -p 6333:6333 -p 6334:6334 \
+    -v $(pwd)/qdrant_storage:/qdrant/storage:z \
+    qdrant/qdrant
+
+## Try out the example scripts
+
+
+LangChain Chain
+```
+python langchain-chain-ollama.py
+```
+
+Llama_Index RAG chatbot over Ben Franklin's writings
+```
+python llama_index-rag-ollama.py
+```
+
+LangChain Agent with Duck Duck Go search api
+Note: Chose Duck Duck Go search API because it is the only of the big ones with a keyless API
+```
+python langchain-agent-ollama.py
+```
+
+
+
+## Want to contribute? 
+
+Would love to accept some contributions or requests for other examples you'd like to see. I am running all this on my personal hardware and trying to come up with fun and useful examples for myself.
---
+++ lesson-1-Advanced-RAG-Pipeline.py
@@ -0,0 +1,235 @@
+# Credit:
+# Much code from this section came from these sources, check them out too
+# https://learn.deeplearning.ai/courses/building-evaluating-advanced-rag/lesson/3/rag-triad-of-metrics
+# https://www.thequalityduck.co.uk/testing-ai-how-to-assess-llm-quality-with-trulens/
+
+from llama_index.llms.ollama import Ollama
+
+### Configs ###
+# Load model configurations from .env file
+# Nice to keep configs in one place to ensure model stays same across files. 
+# Changing model takes a long time for first load
+from dotenv import load_dotenv
+import os
+import logging
+import sys
+
+# Use load dot env to load the .env file and overwrite the os.environ variables
+load_dotenv()
+
+# Configure logging for detailed output. Adjust or disable for less verbosity.
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+model=os.getenv("MODEL")
+base_url=os.getenv("BASE_OLLAMA_URL")
+context_window_size=os.getenv("CONTEXT_WINDOW_SIZE")
+################
+
+print(f"Model: {model}")
+print(f"base_url: {base_url}")
+print(f"context_window_size: {context_window_size}")
+
+####################################################################################
+# Initialize Ollama Model
+####################################################################################
+# Setup Ollama model with specific configurations for use in chat interactions.
+print("Configuring Ollama...")
+from llama_index.core import Settings
+Settings.llm = Ollama(
+    model=model,
+    base_url=base_url,
+    request_timeout=120  # Adjust timeout for model responsiveness.
+)
+Settings.context_window = int(context_window_size)
+
+# Set embeddings model
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+print("Configuring Local Embeddings Models...")
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+
+####################################################################################
+### Prepare Data ###
+####################################################################################
+
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+print("Loading data...")
+
+documents = SimpleDirectoryReader(
+    input_files=["./data/eBook-How-to-Build-a-Career-in-AI.pdf"]
+).load_data()
+
+print(f"Loaded {len(documents)} documents")
+
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+####################################################################################
+# Query the Model with naive rag
+####################################################################################
+
+
+# print("\n#######\nQuerying the model...\n")
+# query = "What are steps to take when finding projects to build your experience?"
+# print(f"Query: {query}")
+# response = query_engine.query(query)
+# print(f"Response: {response}")
+# print("\n#######\n")
+
+
+####################################################################################
+### Sentence Window ###
+####################################################################################
+
+from llama_index.core import Document
+
+document = Document(text="\n\n".\
+                    join([doc.text for doc in documents]))
+
+
+
+
+####################################################################################
+# Evals using Trulens using ollama as the LLM provider #
+####################################################################################
+
+from trulens_eval import Tru
+from trulens_eval import (
+    Feedback,
+    TruLlama
+)
+from trulens_eval.feedback import GroundTruthAgreement
+from trulens_eval.feedback.provider.litellm import LiteLLM
+import numpy as np
+
+print("Evaluating the model...\n\n#######\n")
+tru = Tru()
+tru.reset_database() # This is needed in order to resolve some bug with setting the right ollama url
+
+
+litellm_provider = LiteLLM()
+provider = LiteLLM(
+    model_engine=f"ollama/{model}", 
+    endpoint=base_url
+)
+
+f_qa_relevance = Feedback(
+    provider.relevance_with_cot_reasons,
+    name="Answer Relevance"
+).on_input_output()
+
+
+
+tru_recorder = TruLlama(
+    query_engine,
+    app_id="Direct Query Engine",
+        feedbacks=[
+        f_qa_relevance
+    ]
+)
+
+
+eval_questions = []
+# with open('eval_questions.txt', 'r') as file:
+#     for line in file:
+#         # Remove newline character and convert to integer
+#         item = line.strip()
+#         eval_questions.append(item)
+
+eval_questions.append("How can I be successful in AI?")
+
+print(eval_questions)
+
+with tru_recorder as recording:
+    for question in eval_questions:
+        response = query_engine.query(question)
+
+
+#### Old Stuff ###############################################################
+
+# eval_questions = []
+# with open('eval_questions.txt', 'r') as file:
+#     for line in file:
+#         # Remove newline character and convert to integer
+#         item = line.strip()
+#         print(item)
+#         eval_questions.append(item)
+
+# tru = Tru()
+# tru.reset_database() # This is needed in order to resolve some bug with setting the right ollama url
+
+# litellm_provider = LiteLLM()
+# provider = LiteLLM(
+#     model_engine=f"ollama/{model}", 
+#     endpoint=base_url
+# )
+
+# qa_relevance = (
+#     Feedback(provider.relevance_with_cot_reasons, name="Answer Relevance")
+#     .on_input_output()
+# )
+
+# qs_relevance = (
+#     Feedback(provider.relevance_with_cot_reasons, name = "Context Relevance")
+#     .on_input()
+#     .on(TruLlama.select_source_nodes().node.text)
+#     .aggregate(np.mean)
+# )
+
+# # ToDo: Get Groundedness working
+# from trulens_eval.feedback import Groundedness
+# grounded = Groundedness(groundedness_provider=provider)
+
+# groundedness = (
+#     Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
+#         .on(TruLlama.select_source_nodes().node.text)
+#         .on_output()
+#         .aggregate(grounded.grounded_statements_aggregator)
+# )
+
+# f_groundedness = (
+#     Feedback(provider.groundedness_measure_with_cot_reasons, name = "Groundedness")
+#     .on(TruLlama.select_source_nodes().node.text)
+#     .on_output()
+#     .aggregate(grounded.grounded_statements_aggregator)
+# )
+
+# f_groundedness = (
+#     Feedback(provider.groundedness_measure_with_cot_reasons, name = "Groundedness")
+#     .on(Select.RecordCalls.retrieve.rets.collect())
+#     .on_output()
+# )
+
+# huggingface_provider = Huggingface()
+# groundedness_hug = Groundedness(groundedness_provider=huggingface_provider)
+# f_groundedness_hug = Feedback(groundedness_hug.groundedness_measure, name = "Groundedness Huggingface").on_input().on_output().aggregate(groundedness_hug.grounded_statements_aggregator)
+# def wrapped_groundedness_hug(input, output):
+#     return np.mean(list(f_groundedness_hug(input, output)[0].values()))
+
+# # feedbacks = [qa_relevance, qs_relevance, groundedness]
+# feedbacks = [qa_relevance, qs_relevance]
+# feedbacks = [qa_relevance]
+
+# def get_prebuilt_trulens_recorder(query_engine, app_id):
+#     tru_recorder = TruLlama(
+#         query_engine,
+#         app_id=app_id,
+#         feedbacks=feedbacks
+#         )
+#     return tru_recorder
+
+# tru_recorder = get_prebuilt_trulens_recorder(query_engine,
+#                                              app_id="Direct Query Engine")
+
+# with tru_recorder as recording:
+#     for question in eval_questions:
+#         response = query_engine.query(question)
+
+# records, feedback = tru.get_records_and_feedback(app_ids=[])
+
+# records.head()
+
+# # launches on http://localhost:8501/
+# tru.run_dashboard()
